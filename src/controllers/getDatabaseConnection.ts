f1f585b47e5c122112165706596cbe3068dac311
mohammed412anas <mohammed.anas@everest.engineering>
2026-02-01 16:47:01 +0530

    Authenticate and connect with database

---
+++ src/controllers/getDatabaseConnection.ts
@@ -0,0 +1,13 @@
+import type { Request, Response } from "express";
+import sequelize from "../config/index.ts";
+
+const getDatabaseConnection = async (req: Request, res: Response) => {
+  try {
+    await sequelize.authenticate();
+    res.status(200).send("Connection has been established successfully.");
+  } catch (error) {
+    res.status(500).send("Unable to establish connection.");
+  }
+};
+
+export default getDatabaseConnection;
