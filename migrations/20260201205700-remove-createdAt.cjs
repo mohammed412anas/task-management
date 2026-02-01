68243896eeea55913e8cf0003840c0be810c6421
mohammed412anas <mohammed.anas@everest.engineering>
2026-02-02 02:31:50 +0530

    Remove createdAt attribute from people

---
+++ migrations/20260201205700-remove-createdAt.cjs
@@ -0,0 +1,15 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("People", "createdAt");
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.addColumn("People", "createdAt", {
+      allowNull: false,
+      type: Sequelize.DATE,
+    });
+  },
+};
