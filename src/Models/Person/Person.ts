94d371d30540b6760127ebda6c96b4fb48889681
mohammed412anas <mohammed.anas@everest.engineering>
2026-02-01 23:52:23 +0530

    Define Person model as record of People table

---
+++ src/Models/Person/Person.ts
@@ -0,0 +1,31 @@
+import Sequelize from "sequelize";
+import sequelize from "../../config/index.js";
+
+const Person = sequelize.define("People", {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+
+  createdAt: {
+    allowNull: false,
+    type: Sequelize.DATE,
+  },
+  updatedAt: {
+    allowNull: false,
+    type: Sequelize.DATE,
+  },
+});
+
+export default Person;
