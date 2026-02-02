dfa3335d14da346aaf089b2f4344119ba64e8164
mohammed412anas <mohammed.anas@everest.engineering>
2026-02-02 17:25:36 +0530

    Add get method to get request to fetch tasks created by specific person from client

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -4,6 +4,7 @@ import getPeople from "../controllers/getPeople.js";
 import deactivatePerson from "../controllers/deactivatePerson.js";
 import getActivePeople from "../controllers/getActivePeople.js";
 import createTask from "../controllers/createTask.js";
+import getTaskOfPerson from "../controllers/getTaskOfPerson.js";
 
 const router: IRouter = express.Router();
 
@@ -12,5 +13,6 @@ router.get("/getPeople", getPeople);
 router.put("/deactivatePerson/:id", deactivatePerson);
 router.get("/getActivePeople", getActivePeople);
 router.post("/createTask", createTask);
+router.get("/getTaskOfPerson/:personId", getTaskOfPerson);
 
 export default router;
