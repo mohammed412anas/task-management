9dd031809dea317515c27949ad5ffc39c9fb4502
mohammed412anas <mohammed.anas@everest.engineering>
2026-02-02 10:21:52 +0530

    Wrap deactivateRecord service into try catch block

--- src/services/deactivateRecord/deactivateRecord.ts
+++ src/services/deactivateRecord/deactivateRecord.ts
@@ -1,14 +1,19 @@
+import type { Error } from "sequelize";
 import Person from "../../models/Person/Person.js";
 import type { DeactivateRecord } from "./deactivateRecordType.js";
 
 const deactivateRecord: DeactivateRecord = async (id) => {
-  const deactivatePerson = await Person.update(
-    { status_active: false },
-    { where: { id: id } },
-  );
-  if (deactivatePerson[0])
-    return `Updated the status of person with id : ${id} status_active as false.`;
-  else return `Person with id: ${id} not exists.`;
+  try {
+    const deactivatePerson = await Person.update(
+      { status_active: false },
+      { where: { id: id } },
+    );
+    if (deactivatePerson[0])
+      return `Updated the status of person with id : ${id} status_active as false.`;
+    else return `Person with id: ${id} not exists.`;
+  } catch (error) {
+    return error as Error;
+  }
 };
 
 export default deactivateRecord;
